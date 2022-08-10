06e1ec64373dbcb7df67e879798d3a9e5bf39375
deracali <chideracalistus00@gmail.com>
2022-08-09 22:52:53 -1100

    initial commit

---
+++ Personal-website/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/header/header'
+// import Navbar from './components/nav/Nav'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-circle1'></div>
+      <div className='bg-circle2'></div>
+      <Header />
+      {/* <Navbar /> */}
+    </>
+  )
+}
+
+export default App
---
+++ Personal-website/src/components/header/header.jsx
@@ -0,0 +1,17 @@
+import './header.css'
+
+const Header = () => {
+    return (
+        <div id='home' className='container header-container'>
+            <h3>
+                <p>
+                    I <span>'</span>
+                </p>
+                Samuel
+                <h2>Front-End Web Developer</h2>
+            </h3>
+        </div>
+    );
+}
+
+export default Header;
